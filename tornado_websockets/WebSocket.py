96a32a9edc0b9f98102d802abe3fa0457f2eede5
Hugo ALLIAUME <kocal@live.fr>
2016-05-03 16:03:19 +0200

    v0.0.2
    Django app setup is done and so is runtornado command.
    I can now start to develop a wrapper for tornado.websocket.WebSocketHandler

--- tornado_websockets/WebSocket.py
+++ tornado_websockets/WebSocket.py
@@ -2,4 +2,12 @@ import tornado.websocket
 
 
 class WebSocket(tornado.websocket.WebSocketHandler):
-    pass
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        print("Someone said: %s" % message)
+        self.write_message(u"You said: %s" % message)
+
+    def on_close(self):
+        print("WebSocket closed")
