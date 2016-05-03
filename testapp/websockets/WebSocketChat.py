7454196b87a4bc559f67bf092ac490127d9af2f9
Hugo ALLIAUME <kocal@live.fr>
2016-05-03 16:54:57 +0200

    Ready to test WebSocket.on(evt, fn(data)) method

--- testapp/websockets/WebSocketChat.py
+++ testapp/websockets/WebSocketChat.py
@@ -2,4 +2,10 @@ from tornado_websockets.WebSocket import WebSocket
 
 
 class WebSocketChat(WebSocket):
-    pass
+    @WebSocket.on('connection')
+    def on_connection(self, data):
+        pass
+
+    @WebSocket.on('foo')
+    def on_foo(self, data):
+        pass
--- tornado_websockets/WebSocket.py
+++ tornado_websockets/WebSocket.py
@@ -1,13 +1,29 @@
+import tornado.escape
 import tornado.websocket
 
 
 class WebSocket(tornado.websocket.WebSocketHandler):
-    def open(self):
-        print("WebSocket opened")
+    CHANNEL_DEFAULT = 'default'
+
+    def __init__(self, *args, **kwargs):
+        super(WebSocket, self).__init__(*args, **kwargs)
+
+        self._channels = {self.CHANNEL_DEFAULT: []}
+        self._events = {}
 
     def on_message(self, message):
-        print("Someone said: %s" % message)
-        self.write_message(u"You said: %s" % message)
 
-    def on_close(self):
-        print("WebSocket closed")
+        print('Message received: %s' % str(message))
+
+        try:
+            message = tornado.escape.json_decode(message)
+            event = message.get('event', 'message')
+            data = message.get('data', {})
+        except ValueError:  # It's a JSON
+            event = 'message'
+            data = {'message': message}
+
+        self.write_message('Event: %s\nData: %s' % (event, str(data)))
+
+    def on(self):
+        pass
