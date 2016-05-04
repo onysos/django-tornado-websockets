812ba12496d96e5a9b9f908eb51c397b1fbc5995
Hugo ALLIAUME <kocal@live.fr>
2016-05-04 14:20:25 +0200

    Implementation WebSocket.(setup|register_events|on|emit)()

--- tornado_websockets/WebSocket.py
+++ tornado_websockets/WebSocket.py
@@ -5,15 +5,81 @@ import tornado.websocket
 class WebSocket(tornado.websocket.WebSocketHandler):
     CHANNEL_DEFAULT = 'default'
 
+    instances = []
+
     def __init__(self, *args, **kwargs):
         super(WebSocket, self).__init__(*args, **kwargs)
-
-        self._channels = {self.CHANNEL_DEFAULT: []}
         self._events = {}
+        self.channels = {self.CHANNEL_DEFAULT: []}
+
+        self.setup()
+
+    def setup(self):
+        self.register_events()
+
+    def register_events(self):
+        """
+        Should be implemented by a
+        :return:
+        """
+        raise NotImplementedError
+
+    def on(self, *args):
+        """
+        Bind easily an event on a WebSocket method
+
+        Example:
+            @self.on
+            def my_event(...):
+                ...
+
+            @self.on('my_event')
+            def another_function(...):
+                ...
+
+        :param args: Event name or a function
+        :return:
+        """
+
+        def decorator(func):
+            def wrapper(self, *args, **kwargs):
+                print('wrapper(): %s, %s' % (str(*args), str(**kwargs)))
+                return func(self, *args, **kwargs)
+
+            return wrapper
+
+        if len(args) == 0:
+            raise ValueError("WebSocket.on decorator should have at least one argument")
+
+        # args[0] is a string
+        event = args[0]
+        callback = decorator
+
+        # args[0] is a function
+        if callable(args[0]):
+            event = args[0].__name__
+            callback = decorator(args[0])
+
+        self._events[event] = callback
+        print('events: %s' % str(self._events))
+
+        return callback
+
+    def emit(self, event, data):
+        print('-- WebSocket.emit(%s, %s)' % (event, data))
+
+        message = {
+            'event': event,
+            'data': data
+        }
+
+        message = tornado.escape.json_encode(message)
+        self.write_message(message)
+
+        pass
 
     def on_message(self, message):
-
-        print('Message received: %s' % str(message))
+        print('-- Message received: %s' % str(message))
 
         try:
             message = tornado.escape.json_decode(message)
@@ -23,7 +89,7 @@ class WebSocket(tornado.websocket.WebSocketHandler):
             event = 'message'
             data = {'message': message}
 
-        self.write_message('Event: %s\nData: %s' % (event, str(data)))
-
-    def on(self):
-        pass
+        if not self._events.get(event):
+            print('-- Warning: %s event is not implemented' % event)
+        else:
+            self._events[event](data)
