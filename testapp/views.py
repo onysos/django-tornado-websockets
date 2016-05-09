ebd5e08b6a6bb5dbfe921bfb4eb57c25e785abb2
Hugo ALLIAUME <kocal@live.fr>
2016-05-09 16:22:21 +0200

    Add some docstring to testapp/views

--- testapp/views.py
+++ testapp/views.py
@@ -4,28 +4,41 @@ from tornado_websockets.websocket import WebSocket
 
 ws_chat = WebSocket('/my_chat')
 
-print('WS_CHAT: %s' % ws_chat)
-
 
 class MyChat(TemplateView):
+    """
+        Proof of concept about a really simple web chat using websockets
+    """
+
     template_name = 'testapp/index.html'
 
     def __init__(self, **kwargs):
         super(MyChat, self).__init__(**kwargs)
-        ws_chat.context = self
+
+        ws_chat.context = self  # Otherwise, 'self' parameter for method decorated by @ws_chat.on will not be defined
 
     @ws_chat.on
     def connection(self, data):
+        """
+            Called when the client send the event "connection".
+
+            :param data: Data sent by the client
+            :type data: dict
+            :return: None
+        """
         ws_chat.emit('new_connection', '%s just joined the webchat' % data.get('username', '<Anonymous>'))
 
-    # @ws_chat.on('message')
     @ws_chat.on
     def message(self, data):
+        """
+            Called when the client send a new message
+
+            :param data: Data sent by the client
+            :type data: dict
+            :return: None
+        """
+
         ws_chat.emit('new_message', {
             'username': data.get('username', '<Anonymous>'),
             'message': data.get('message', 'Empty message')
         })
-
-    @ws_chat.on
-    def close(self, data):
-        pass
