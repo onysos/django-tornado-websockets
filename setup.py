7a7b62c99c35927adfd92382fc1fac8c6382bac7
Hugo ALLIAUME <kocal@live.fr>
2016-05-14 21:08:39 +0200

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tornado-websockets',
-    version='0.0.4',
+    version='0.0.5',
     description="Simple way to use WebSockets for Django with Tornado",
     long_description=README,
     author='Hugo ALLIAUME',
@@ -23,7 +23,7 @@ setup(
     include_package_data=True,
     license='GPLv3 License',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
@@ -32,7 +32,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
