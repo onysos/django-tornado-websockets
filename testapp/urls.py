445680a8b1db4cbb0e473e30ac672a225ab17e31
Hugo ALLIAUME <kocal@live.fr>
2016-05-26 16:26:48 +0200

    Module/ProgressBar: add an example page in testapp

--- testapp/urls.py
+++ testapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^my_chat/$', views.MyChat.as_view(), name='index'),
+    url(r'^my_progress_bar/$', views.MyProgressBar.as_view(), name='progress_bar'),
     url(r'^admin/', admin.site.urls)
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,9 +1,14 @@
+import threading
+import time
+
 from django.views.generic import TemplateView
 
+from tornado_websockets.modules.progress_bar import ProgressBar
 from tornado_websockets.websocket import WebSocket
 
 # WebSocket echo server
 ws_echo = WebSocket('/echo')
+
 
 @ws_echo.on
 def message(socket, data):
@@ -11,8 +16,9 @@ def message(socket, data):
 
 
 # WebSocket chat server, using Django for template rendering
-
 ws_chat = WebSocket('/my_chat')
+
+
 class MyChat(TemplateView):
     """
         Proof of concept about a really simple web chat using websockets and supporting messages history
@@ -72,3 +78,27 @@ class MyChat(TemplateView):
 
         self.messages = []
 
+
+# WebSocket module progress bar
+ws_pb = ProgressBar('/my_progress_bar', max=100)
+
+
+class MyProgressBar(TemplateView):
+    template_name = 'testapp/progress_bar.html'
+
+    def __init__(self, **kwargs):
+        super(MyProgressBar, self).__init__(**kwargs)
+        ws_pb.context = self
+
+    @ws_pb.on
+    def go(self, socket):
+        ws_pb.reset()
+        self.start_thread()
+
+    def start_thread(self):
+        def my_function():
+            for value in range(0, ws_pb.max):
+                time.sleep(.1)
+                ws_pb.tick(label="[%d/%d] Tâche %d terminée" % (ws_pb.value, ws_pb.max, value))
+
+        threading.Thread(None, my_function, None).start()
